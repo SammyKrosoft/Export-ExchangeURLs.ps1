4eb108025d6f5d220bec02c3f41ed05f318a13f7
SammyKrosoft <sammy@hotmail.fr>
2019-06-26 23:27:18 -0400

    Added NTLM Ext auth

--- Import-ExchangeURLs.ps1
+++ Import-ExchangeURLs.ps1
@@ -512,10 +512,10 @@ Foreach ($CurrentServer in $ServersConfigs) {
     }
     If (IsNotEmpty $CurrentServer."OutlookAnywhere-ExternalHostNAme(E2010+)"){
         If ($CurrentServer.ServerVersion -match "15\."){
-        $OAcmd += " -ExternalHostName $($CurrentServer."OutlookAnywhere-ExternalHostNAme(E2010+)") -ExternalClientsRequireSsl `$true -DefaultAuthenticationMethod Ntlm"
+        $OAcmd += " -ExternalHostName $($CurrentServer."OutlookAnywhere-ExternalHostNAme(E2010+)") -ExternalClientsRequireSsl `$true -ExternalClientAuthenticationMethod Negotiate  -IISAuthenticationMethods @('Ntlm', 'Basic', 'Negotiate')"
         } Elseif ($CurrentServer.ServerVersion -match "14\."){
             # If Exchange 2010 server, then we set ExternalHostName, but without -ExternalClientsRequireSSL switch (that switch is for E2013/2016/2019 only)
-            $OAcmd += " -ExternalHostName $($CurrentServer."OutlookAnywhere-ExternalHostNAme(E2010+)")"
+            $OAcmd += " -ExternalHostName $($CurrentServer."OutlookAnywhere-ExternalHostNAme(E2010+)") -ExternalClientAuthenticationMethod NTLM"
         }
     } Else {
         $OAcmd += " -ExternalHostName `$null"
